b1b22b1dea51f711efc8d5b737664315949d29f3
loganmedina2 <loganmedina2@gmail.com>
2018-10-15 18:30:47 -0600

    float list

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
+def float_to_int(float_list):
+    int_list = []
+    for f in float-list:
+        int_list.append(int(f))
+        return int_list
 
 
 
 
 
 if __name__ == '__main__':
-    print("Hello World!"
+    floats = [float(i0 for i in range(100))]
+    print(float_to_int(float))
